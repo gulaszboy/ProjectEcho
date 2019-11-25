4518dd8126c0f58eeb74be191289b4fa03845c54
Kamil Adamczyk <kamada37@gmail.com>
2019-11-25 11:43:49 +0100

    Added some basic html for api testing

---
+++ src/users/Form.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+interface FormState {
+    email: string,
+    firstName: string,
+    lastName: string,
+    phone: string,
+    [key: string]: any
+}
+
+export class Form extends React.Component<{}, FormState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            email: "",
+            firstName: "",
+            lastName: "",
+            phone: "",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+        this.setState({ [(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement).value });
+    };
+
+    render() {
+        const { email, firstName, lastName, phone } = this.state;
+
+        return (
+            <>
+                <label htmlFor="firstName">Name</label>
+                <input type="text" id="firstName" placeholder="First Name" name="firstName" value={firstName} onChange={this.handleChange} />
+
+                <label htmlFor="lastName">Name</label>
+                <input type="text" id="lastName" placeholder="Last Name" name="lastName" value={lastName} onChange={this.handleChange} />
+
+                <label htmlFor="email">Name</label>
+                <input type="text" id="email" placeholder="Email" name="email" value={email} onChange={this.handleChange} />
+
+                <label htmlFor="phone">Name</label>
+                <input type="text" id="phone" placeholder="Phone" name="phone" value={phone} onChange={this.handleChange} />
+            </>
+        )
+    }
+}
+
+export default Form;
---
+++ src/users/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface User {
+    firstName: string,
+    lastName: string,
+    email: string,
+}
+
+type UserListState = {
+    users: Array<User>
+}
+
+export class UserList extends React.Component<{}, UserListState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("api/users")
+            .then(resp => resp.json())
+            .then(resp => this.setState({ users: resp.users }))
+    }
+
+    render() {
+        const { users } = this.state;
+
+        if (users.length === 0) return (<h1>Please wait</h1>)
+
+        const size = 10;
+        const list = users.filter((u, i) => i < size).map(user =>
+            <p>
+                {user.firstName} : {user.lastName} : {user.email}
+                <button>View</button>
+                <button>Edit</button>
+                <button>Delete</button>
+            </p>)
+
+        return (
+            <>
+                <button>New user</button>
+                {list}
+            </>
+        );
+    }
+
+}
+
+export default UserList;
