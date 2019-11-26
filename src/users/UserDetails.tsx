31d93929ddd72e08cd9af86a5869f4f96e94ac2f
Kamil Adamczyk <kamada37@gmail.com>
2019-11-26 13:06:28 +0100

    Posts list added to UserDispaly

--- src/users/UserDetails.tsx
+++ src/users/UserDetails.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import { User } from '.';
 import { Link } from 'react-router-dom';
+import { Post } from '../posts';
 
 type Props = {
     user: User,
 }
 
-export class UserDetails extends React.Component<Props> {
+type State = {
+    posts: Array<Post>
+}
+
+export class UserDetails extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = { posts: [] }
+    }
+
+    componentDidMount() {
+        const { user } = this.props;
+        fetch(`http://localhost:8081/api/users/${user._id}`)
+            .then(resp => resp.json())
+            .then(resp => this.setState({ posts: resp.posts }))
+    }
+
     render() {
         const { email, firstName, lastName, phone, address } = this.props.user
+        const { posts } = this.state;
+
+        const postList = posts.map(p => (
+            <div>
+                <div>{p.title}</div>
+                <div>{p.body}</div>
+            </div>
+        ))
+
         return (
             <>
                 <h1>User details</h1>
@@ -18,6 +44,8 @@ export class UserDetails extends React.Component<Props> {
                 <div>{address}</div>
 
                 <Link to="/users">Return</Link>
+
+                {postList}
             </>
         )
     }
